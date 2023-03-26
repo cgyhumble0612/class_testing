ecc685207d5a8977c0402de47e688614c4b65d75
cgyhumble0612 <cgyhumble@uchicago.edu>
2023-03-26 17:58:09 -0500

    Create example

---
+++ say_hi.py
@@ -0,0 +1,9 @@
+"""
+-*- coding: utf-8 -*-
+
+@Author : cgyhumble
+@Time : 2023/3/26 17:57
+@File : say_hi.py
+"""
+if __name__ == '__main__':
+    print("Hi everybody, I'm a graduate student from UChicago MACSS program")
